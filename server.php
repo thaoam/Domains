28f0fecd886c419bcdd0b8a90f617c168123eee6
tiepcreator <96404465+tiepcreator@users.noreply.github.com>
2022-01-09 13:58:36 +0700

    Update server.php

--- server.php
+++ server.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 $results = '';
 // get or generate your key at https://www.dynadot.com/account/domain/setting/api.html
-$key = 'your-api-key';
+$key = '8J6LQ6C6b7E9QG7Z8x8qz8dK8B8ZN8i6m8O7U7pC7u';
 foreach (explode(",", $_GET['domains']) as $domain) {
     foreach (explode(",", $_GET['domainexts']) as $ext) {
         $results .= file_get_contents("https://api.dynadot.com/api3.xml?key={$key}&command=search&domain0={$domain}.{$ext}");
